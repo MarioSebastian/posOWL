37f8ba43c6b05dd5ccaee835aa1bc4e571493444
mariosebastian <mariosebastian@owleyewear.id>
2024-10-09 19:31:04 +0700

    - third push

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
-# Vue 3 + Vite
-
-This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
-
-Learn more about IDE Support for Vue in the [Vue Docs Scaling up Guide](https://vuejs.org/guide/scaling-up/tooling.html#ide-support).
+1. The entry point of any Electron application is its main script. This script controls the main process, which runs in a full Node.js environment and is responsible for controlling your app's lifecycle, displaying native interfaces, performing privileged operations, and managing renderer processes (more on that later). During execution, Electron will look for this script in the main field of the app's package.json config, which you should have configured during the app scaffolding step.
+2. 
--- electron/main.js
+++ electron/main.js
@@ -1,31 +1,31 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+import { app, BrowserWindow } from "electron";
+import path from "path"
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // Create a new BrowserWindow when `app` is ready
 const createWindow = () => {
+
     const mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
             contextIsolation: true,
             enableRemoteModule: false,
-            nodeIntegration: false,
+            preload: path.join(__dirname, "preload.js")
         },
     });
 
     // Load the Vite server in development or the index.html in production
-    if (app.isPackaged) {
-        mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
-    } else {
-        mainWindow.loadURL('http://localhost:3000');
-    }
+    app.isPackaged
+        ? mainWindow.loadFile(path.join(__dirname, "index.html")) // Prod
+        : mainWindow.loadURL("app://./index.html"); // Dev
 };
 
 // Quit the app when all windows are closed (except on macOS)
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
-});
+    if (process.platform !== 'darwin') app.quit()
+})
 
 // Create a new window when the app is activated (macOS)
 app.on('activate', () => {
